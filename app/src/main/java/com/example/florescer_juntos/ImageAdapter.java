7285e981a64c91a76bdfa5461cdb366a4372e91d
Matheus <jmatheus17@gmail.com>
2024-03-09 23:14:51 -0300

    Listagem de Posts na Home, no perfil e filtro por tipo

--- app/src/main/java/com/example/florescer_juntos/ImageAdapter.java
+++ app/src/main/java/com/example/florescer_juntos/ImageAdapter.java
@@ -1,7 +1,10 @@
 package com.example.florescer_juntos;
 
 import android.content.Context;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -17,11 +20,14 @@ import java.util.List;
 
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
     private Context mContext;
-    private List<Post> mPosts;
+    private static List<Post> mPosts;
+    private static String mUserId;
+    private static OnItemClickListener mListener;
 
-    public ImageAdapter(Context mContext, List<Post> mPosts) {
+    public ImageAdapter(Context mContext, List<Post> mPosts, String userId) {
         this.mContext = mContext;
         this.mPosts = mPosts;
+        this.mUserId = userId;
     }
 
     @NonNull
@@ -34,11 +40,14 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
     @Override
     public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
         Post postCurent = mPosts.get(position);
-        holder.nomeView.setText(postCurent.getIdUsuario().toString());
+        holder.nomeView.setText(postCurent.getIdUsuario());
         Glide.with(mContext)
-                .load(postCurent.getImageUrl()).centerInside()
+                .load(postCurent.getImageUrl()).placeholder(R.mipmap.ic_launcher)
+                .centerInside()
                 .into(holder.imageView);
         holder.descView.setText(postCurent.getDescricao());
+        String tipo = "Tipo: "+postCurent.getTipoPlanta();
+        holder.tipoView.setText(tipo);
     }
 
     @Override
@@ -46,8 +55,9 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
         return mPosts.size();
     }
 
-    public static class ImageViewHolder extends RecyclerView.ViewHolder{
-        public TextView nomeView, descView;
+    public static class ImageViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,
+            View.OnCreateContextMenuListener, MenuItem.OnMenuItemClickListener {
+        public TextView nomeView, descView, tipoView;
         public ImageView imageView;
 
         public ImageViewHolder(View itemView){
@@ -56,7 +66,63 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
             nomeView = itemView.findViewById(R.id.tvNomeUsuario);
             descView = itemView.findViewById(R.id.tvPostDescricao);
             imageView = itemView.findViewById(R.id.ivImagePost);
+            tipoView = itemView.findViewById(R.id.tvPostTipo);
+
+            itemView.setOnClickListener(this);
+            itemView.setOnCreateContextMenuListener(this);
         }
+
+        @Override
+        public void onClick(View v){
+            if (mListener != null){
+                int position = getAdapterPosition();
+                if(position != RecyclerView.NO_POSITION){
+                    mListener.onItemClick(position);
+                }
+            }
+        }
+
+        @Override
+        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+            menu.setHeaderTitle("Escolha uma ação");
+            MenuItem save = menu.add(Menu.NONE, 1, 1, "Salvar post");
+            save.setOnMenuItemClickListener(this);
+            // Verificar se o ID do usuário logado corresponde ao ID do usuário associado ao post
+            if (mPosts.get(getAdapterPosition()).getIdUsuario().equals(mUserId)) {
+                MenuItem delete = menu.add(Menu.NONE, 2, 2, "Deletar post");
+                delete.setOnMenuItemClickListener(this);
+            }
+        }
+
+        @Override
+        public boolean onMenuItemClick(@NonNull MenuItem item) {
+            if (mListener != null){
+                int position = getAdapterPosition();
+                if(position != RecyclerView.NO_POSITION){
+                    if(item.getItemId()==1){
+                        mListener.onSaveClick(position);
+                        return true;
+                    } else if (item.getItemId()==2) {
+                        mListener.onDeleteClick(position);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+
+        void onSaveClick(int position);
+
+        void onDeleteClick(int position);
+
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        mListener = listener;
     }
 
 }
